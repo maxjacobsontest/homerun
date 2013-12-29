ecf070d4154ae801c1665f12127c5913e6813136
Max Jacobson <max@hardscrabble.net>
2013-12-29 20:10:31 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homerun::Application.config.secret_key_base = '89b4650528dab45798a04f81ca264266f830a6a29f8a2c219040e075613dfa53af34923db02c2f57dce35bd7991a4322a73a7516fa363cbf725aabc02c11f737'
