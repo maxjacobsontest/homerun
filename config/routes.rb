7ca89492d50127527371bd879efac903b45391e5
Max Jacobson <max@hardscrabble.net>
2013-12-29 23:50:31 +0000

    root to tasks index

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Homerun::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'tasks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
