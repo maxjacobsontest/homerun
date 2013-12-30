ff69f1344d4331ca0a516272451c63a411b61ba2
Max Jacobson <max@hardscrabble.net>
2013-12-30 02:30:40 +0000

    add total points to tasks index

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ActiveRecord::Base
   belongs_to :course
   belongs_to :category
   validates_presence_of :course, :category, :name, :points
+  def self.remaining_points
+    all.inject(0) { |sum, task| sum + task.points }
+  end
 end
