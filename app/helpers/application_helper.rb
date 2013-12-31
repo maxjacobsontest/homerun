40921b8b9c6b76eaafc2b2318eab3282a9d3c32b
Max Jacobson <max@hardscrabble.net>
2013-12-31 23:01:32 +0000

    add inspirational messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def inspirational_message
+    [
+      "You're a star!",
+      "You kick ass!",
+      "You blow away the competition!",
+      "Keep it going!",
+      "You're on a roll!",
+      "Hey, Einstein!",
+      "Way to go!",
+      "NYU, here you come!"
+    ].sample
+  end
 end
